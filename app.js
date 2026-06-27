fb12cb09c37d3294dff9bdafdb8742c8803535e0
Robert Administrador de contenido <mauxy41@gmail.com>
2026-06-27 09:32:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const video = document.getElementById("camara");
 
+const canvas = document.getElementById("canvas");
+
+const boton = document.getElementById("capturar");
+
+const compartir = document.getElementById("compartir");
+
 navigator.mediaDevices.getUserMedia({
 
 video:{
@@ -12,10 +18,36 @@ facingMode:"user"
 
 video.srcObject=stream;
 
-})
+});
 
-.catch(error=>{
+boton.addEventListener("click",()=>{
 
-alert("No fue posible abrir la cámara.");
+canvas.width=video.videoWidth;
+
+canvas.height=video.videoHeight;
+
+const ctx=canvas.getContext("2d");
+
+ctx.drawImage(video,0,0);
+
+const marco=new Image();
+
+marco.src="marco.png";
+
+marco.onload=()=>{
+
+ctx.drawImage(marco,0,0,canvas.width,canvas.height);
+
+const enlace=document.createElement("a");
+
+enlace.download="Hannah-Lia.png";
+
+enlace.href=canvas.toDataURL("image/png");
+
+enlace.click();
+
+compartir.style.display="inline-block";
+
+}
 
 });
