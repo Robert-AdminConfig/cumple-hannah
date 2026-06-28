709d27b3c5d8d574aa5935c823a469e8d2ee93af
Robert Administrador de contenido <mauxy41@gmail.com>
2026-06-28 07:41:47 -0500

    Update camera.js

--- camera.js
+++ camera.js
@@ -24,3 +24,34 @@ audio:true
 video.srcObject = mediaStream;
 
 }
+document
+.getElementById("photoBtn")
+.addEventListener("click",takePhoto);
+
+function takePhoto(){
+
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+
+const ctx = canvas.getContext("2d");
+
+ctx.drawImage(
+video,
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+canvas.toBlob(async(blob)=>{
+
+await uploadFile(
+blob,
+`foto_${Date.now()}.jpg`
+);
+
+alert("Foto subida");
+
+},"image/jpeg",0.95);
+
+}
