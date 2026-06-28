f2a41a84191166b7618c1f3aacc810be27ba8594
Robert Administrador de contenido <mauxy41@gmail.com>
2026-06-28 07:37:53 -0500

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,26 @@
 
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+
+let mediaStream;
+let mediaRecorder;
+let chunks = [];
+
+startCamera();
+
+async function startCamera(){
+
+mediaStream =
+await navigator.mediaDevices.getUserMedia({
+
+video:{
+facingMode:"environment"
+},
+
+audio:true
+
+});
+
+video.srcObject = mediaStream;
+
+}
