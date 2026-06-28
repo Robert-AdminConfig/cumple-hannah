3f226c5b49ed4b54d741462a8227617d9434c0a1
Robert Administrador de contenido <mauxy41@gmail.com>
2026-06-28 13:42:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const video=document.getElementById("camara");
+const canvas=document.getElementById("canvas");
+const boton=document.getElementById("capturar");
+const compartir=document.getElementById("compartir");
+
+navigator.mediaDevices.getUserMedia({
+
+video:{
+facingMode:"user"
+}
+
+})
+
+.then(stream=>{
+
+video.srcObject=stream;
+
+})
+
+.catch(()=>{
+
+alert("No fue posible abrir la cámara");
+
+});
+
+boton.addEventListener("click",()=>{
+
+canvas.width=video.videoWidth;
+
+canvas.height=video.videoHeight;
+
+const ctx=canvas.getContext("2d");
+
+ctx.drawImage(video,0,0,canvas.width,canvas.height);
+
+const marco=new Image();
+
+marco.src="marco.png";
+
+marco.onload=function(){
+
+ctx.drawImage(marco,0,0,canvas.width,canvas.height);
+
+const imagen=canvas.toDataURL("image/png");
+
+const enlace=document.createElement("a");
+
+enlace.href=imagen;
+
+enlace.download="Hannah-Lia-6-anos.png";
+
+enlace.click();
+
+compartir.style.display="block";
+
+}
+
+});
